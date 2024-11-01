0d28aa5e9492d973b4b03a5dc7ece081d24232f7
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-11-01 12:56:05 -0400

    Add job details retrieval and display logic
    Inject JobService and ActivatedRoute to fetch job details based on job ID from route parameters. Update the template to display job information including title, department, location, job type, date posted, and description.

--- src/app/feature/jobs/job-details/job-details.component.ts
+++ src/app/feature/jobs/job-details/job-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Job } from '../../../shared/model/job.model';
+import { JobService } from '../../../core/service/job.service';
+import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-job-details',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './job-details.component.html',
   styleUrl: './job-details.component.scss',
 })
-export class JobDetailsComponent {}
+export class JobDetailsComponent {
+  #jobService = inject(JobService);
+  #activatedRoute = inject(ActivatedRoute);
+
+  job = signal<Job | undefined>(undefined);
+
+  constructor() {
+    const jobId = +this.#activatedRoute.snapshot.params['id'];
+    this.job.set(this.#jobService.getJob(jobId));
+  }
+}
