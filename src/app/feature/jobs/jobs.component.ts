4c176a1ce311f91c9d7277b4bbbe237cfd8bdcd9
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-11-01 12:50:57 -0400

    Add job details view functionality
    Integrate a detailed view for job postings by injecting Router service and adding a method to navigate to job details. The HTML template was updated to include job title, location, job type, and a "View Details" button.

--- src/app/feature/jobs/jobs.component.ts
+++ src/app/feature/jobs/jobs.component.ts
@@ -1,20 +1,27 @@
 import { Component, inject, signal } from '@angular/core';
 import { JobService } from '../../core/service/job.service';
 import { Job } from '../../shared/model/job.model';
+import { SlicePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.scss',
 })
 export class JobsComponent {
   #jobService = inject(JobService);
+  #router = inject(Router);
 
   jobs = signal<Job[]>([]);
 
   constructor() {
     this.jobs.set(this.#jobService.getJobs());
+  }
+
+  viewJobDetails(id: number) {
+    this.#router.navigate(['/jobs', id]);
   }
 }
