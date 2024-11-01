5e8a3aeb7bf0cfe1aedf0d447f8fee358945c1f0
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-11-01 12:31:42 -0400

    Add ClrNavigationModule and content layout
    Added `ClrNavigationModule` to `header.component.ts` to leverage Clarity's navigation features. Updated `app.component.html` to include a new content area with placeholder text for better page structure. Modified `header.component.html` to use Clarity's navigation level directive for improved navigation hierarchy.

--- src/app/core/component/header/header.component.ts
+++ src/app/core/component/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ClrIconModule } from '@clr/angular';
+import { ClrIconModule, ClrNavigationModule } from '@clr/angular';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ClrIconModule, RouterLink, RouterLinkActive],
+  imports: [ClrIconModule, RouterLink, RouterLinkActive, ClrNavigationModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
