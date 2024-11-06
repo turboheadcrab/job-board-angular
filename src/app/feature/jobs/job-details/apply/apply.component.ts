3779eb88038a128ab923d4b186b38f97974846b3
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-11-06 16:17:28 -0500

    Add dynamic form generation for job application
    Added dynamic form generation in ApplyComponent to handle questions from ApplicationService. Introduced ApplicationService to fetch question data and integrated it in the form. Updated the HTML template to loop through and display questions dynamically.

--- src/app/feature/jobs/job-details/apply/apply.component.ts
+++ src/app/feature/jobs/job-details/apply/apply.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { ClrFormsModule } from '@clr/angular';
+import {
+  FormBuilder,
+  FormRecord,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ApplicationService } from '../../../../core/service/application.service';
+import { Question } from '../../../../shared/model/question.model';
+import { tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-apply',
   standalone: true,
-  imports: [],
+  imports: [ClrFormsModule, FormsModule, ReactiveFormsModule],
   templateUrl: './apply.component.html',
-  styleUrl: './apply.component.scss'
+  styleUrl: './apply.component.scss',
 })
 export class ApplyComponent {
+  #applicationService = inject(ApplicationService);
+  #formBuilder = inject(FormBuilder);
 
+  form: FormRecord = this.#formBuilder.record({});
+
+  id = input.required<string>();
+
+  questions = signal<Question[]>([]);
+
+  constructor() {
+    this.#applicationService
+      .getQuestions()
+      .pipe(
+        tap({
+          next: (questions: Question[]) => this.questions.set(questions),
+        }),
+        takeUntilDestroyed(),
+      )
+      .subscribe();
+    for (const question of this.questions()) {
+      const formControl = this.#formBuilder.control<string>('');
+      this.form.addControl(question.id, formControl);
+    }
+  }
+
+  onSubmit() {
+    console.info('ApplyComponent.onSubmit(), form.value: ', this.form.value);
+  }
 }
---
+++ src/app/shared/model/question.model.ts
@@ -0,0 +1,7 @@
+export interface Question {
+  id: string;
+  label: string;
+  type: string;
+  required: boolean;
+  placeholder: string;
+}
