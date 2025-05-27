79c013d31904459ba66f7ca736e43b59c5a96591
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2025-05-27 08:19:02 -0400

    Add `provideNoopAnimations` to ApplyComponent tests
    This ensures that animations do not interfere with the test environment, improving test stability and predictability. The change helps streamline the testing process for the ApplyComponent.

--- src/app/feature/jobs/job-details/apply/apply.component.spec.ts
+++ src/app/feature/jobs/job-details/apply/apply.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApplyComponent } from './apply.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('ApplyComponent', () => {
   let component: ApplyComponent;
@@ -11,7 +12,11 @@ describe('ApplyComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ApplyComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideNoopAnimations(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ApplyComponent);
